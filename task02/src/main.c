21db93308757e4dd3657a0d0a1e9991927229bc0
Artur Jedruszek <ajedruszek@gmail.com>
2015-07-13 14:53:42 +0200

    dziala juz

--- task02/src/main.c
+++ task02/src/main.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int amount = 0;
+char print = 0;
 
 pthread_mutex_t MTX = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t COND = PTHREAD_COND_INITIALIZER;
@@ -12,40 +14,51 @@ bool PRINT = false;
 void foo(void *p)
 {
 	int i;
+for (i=0; i<amount; ++i)
+{
 	pthread_mutex_lock(&MTX);
-	while(i!=0)
+	while(print!=0)
 		pthread_cond_wait(&COND, &MTX);
 	printf("PING\n");
+	print =1;
+	pthread_cond_signal(&COND);
 	pthread_mutex_unlock(&MTX);
-	i=0;
+}	
     return 0;
 }
 
 void foo2(void *p)
 {
 	int i;
+for (i = 0 ; i <amount; ++i){
+
         pthread_mutex_lock(&MTX);
-        while(i!=1)
+        while(print!=1)
                 pthread_cond_wait(&COND, &MTX);
         printf("PONG\n");
+	print = 0;
+	pthread_cond_signal(&COND);
         pthread_mutex_unlock(&MTX);
-	i=1;
+}
     return 0;
 }
 
 
 
-int main(int c, char** v){
+int main(int c, char* v[]){
 
-pthread_t t = 0;
-pthread_create(&t, 0, foo, 0);
+amount = atoi(v[1]);
+pthread_t tid[2];
+pthread_create(&tid[0], 0, foo, 0);
+pthread_create(&tid[1], 0, foo2, 0);
 
-sleep(5);
 pthread_mutex_lock(&MTX);
-PRINT = true;
+
 pthread_cond_signal (& COND);
 pthread_mutex_unlock (& MTX);
-pthread_join(t, 0);
+pthread_join(tid[0], 0);
+pthread_join(tid[1], 0);
+
 
 return 0;
 }
